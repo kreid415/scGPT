8147ab802fcb5288553aa1e9e06b64ebc807568c
Kendall Reid <kreid415@gmail.com>
2025-02-10 11:28:32 -0500

    fix issues with idx building

--- data/cellxgene/build_soma_idx.py
+++ data/cellxgene/build_soma_idx.py
@@ -6,17 +6,18 @@ from typing import List
 import os
 import argparse
 
-parser = argparse.ArgumentParser(
-                    description='Build soma index list based on query')
+parser = argparse.ArgumentParser(description="Build soma index list based on query")
 
 
-parser.add_argument("--query-name",
+parser.add_argument(
+    "--query-name",
     type=str,
     required=True,
     help="query name to build the index",
 )
 
-parser.add_argument("--output-dir",
+parser.add_argument(
+    "--output-dir",
     type=str,
     required=True,
     help="Directory to store the output idx file",
@@ -33,12 +34,12 @@ def retrieve_soma_idx(query_name) -> List[str]:
 
     with cellxgene_census.open_soma(census_version=VERSION) as census:
         cell_metadata = census["census_data"]["homo_sapiens"].obs.read(
-        value_filter = VALUE_FILTER[query_name],
-        column_names = ["soma_joinid"]
-    )
-    cell_metadata = cell_metadata.concat()
-    cell_metadata = cell_metadata.to_pandas()
+            value_filter=VALUE_FILTER[query_name], column_names=["soma_joinid"]
+        )
+        cell_metadata = cell_metadata.concat()
+        cell_metadata = cell_metadata.to_pandas()
     return cell_metadata["soma_joinid"].to_list()
+
 
 def convert2file(idx_list: List[str], query_name: str, output_dir: str) -> None:
     """
@@ -51,9 +52,10 @@ def convert2file(idx_list: List[str], query_name: str, output_dir: str) -> None:
     file_path = os.path.join(output_dir, f"{query_name}.idx")
 
     # write to the file
-    with open(file_path, 'w') as fp:
+    with open(file_path, "w") as fp:
         for item in idx_list:
             fp.write("%s\n" % item)
+
 
 def build_soma_idx(query_name, output_dir) -> None:
     """
@@ -67,5 +69,3 @@ def build_soma_idx(query_name, output_dir) -> None:
 #     build_soma_idx("heart")
 
 build_soma_idx(args.query_name, args.output_dir)
-
-
