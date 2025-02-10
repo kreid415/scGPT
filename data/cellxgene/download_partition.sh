7d78dbe93c838a4122388fa3ab722d37a29a4dfe
kreid415 <kreid415@protonmail.com>
2025-02-10 15:34:24 -0500

    retry if fails

--- data/cellxgene/download_partition.sh
+++ data/cellxgene/download_partition.sh
@@ -3,7 +3,7 @@ QUERY=$1
 INDEX_DIR=$2
 OUTPUT_DIR=$3
 
-MAX_PARTITION_SIZE=200000
+MAX_PARTITION_SIZE=50000
 
 total_num=`wc -l ${INDEX_DIR}/${QUERY}.idx | awk '{ print $1 }'`
 total_partition=$(($total_num / $MAX_PARTITION_SIZE))
@@ -12,11 +12,22 @@ total_partition=$(($total_num / $MAX_PARTITION_SIZE))
 
 for i in $(seq 0 $total_partition)
 do
-    echo "downloading partition ${i}/${total_partition} for ${QUERY}"
-    python3 ./download_partition.py \
-        --query-name ${QUERY} \
-        --index-dir ${INDEX_DIR} \
-        --output-dir ${OUTPUT_DIR} \
-        --partition-idx ${i} \
-        --max-partition-size ${MAX_PARTITION_SIZE}
+    while true;
+    do
+        echo "downloading partition ${i}/${total_partition} for ${QUERY}"
+        python3 ./download_partition.py \
+            --query-name ${QUERY} \
+            --index-dir ${INDEX_DIR} \
+            --output-dir ${OUTPUT_DIR} \
+            --partition-idx ${i} \
+            --max-partition-size ${MAX_PARTITION_SIZE}
+        
+        ret=$?
+        if [ $ret -eq 0 ]; then
+            echo "downloaded succesfully"
+            break
+        else
+            echo "error, retrying"
+        fi
+    done
 done
